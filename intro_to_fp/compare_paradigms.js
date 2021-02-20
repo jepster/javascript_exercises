bad79f714ac69a3388ce7c9081202dbd5e51af57
Peter Majmesku <majmesku@publicplan.de>
2021-02-20 19:35:42 +0100

    Re-written imperative code into object-oriented

--- intro_to_fp/compare_paradigms.js
+++ intro_to_fp/compare_paradigms.js
@@ -1,18 +1,52 @@
 // Directions: Rewrite the imperative code below as Object-Oriented 
 
-let status = 'active'
-let warp = 2
-let type = 'Dilithium Crystal'
-let status_report = 'Captain, '
+// let status = 'active'
+// let warp = 2
+// let type = 'Dilithium Crystal'
+// let status_report = 'Captain, '
+//
+// if(status === 'active' && warp <= 4) {
+//     status_report += 'the engines are active and we could be going faster.'
+// } else if (status === 'active' && warp > 4) {
+//     status_report += 'the engines are active and we are going ' + warp + '.'
+// } else if (status === 'down') {
+//     status_report += 'the engines are down.'
+// } else {
+//     status_report += 'the comms are down and we can`t reach engineering.'
+// }
+//
+// console.log(status_report)
 
-if(status === 'active' && warp <= 4) {
-    status_report += 'the engines are active and we could be going faster.'
-} else if (status === 'active' && warp > 4) {
-    status_report += 'the engines are active and we are going ' + warp + '.'
-} else if (status === 'down') {
-    status_report += 'the engines are down.'
-} else {
-    status_report += 'the comms are down and we can`t reach engineering.'
+
+class StatusReport {
+    constructor(name){
+        this.name = name
+    }
+
+    determineStatus(status = 'active'){
+        let warp = 2
+        let type = 'Dilithium Crystal'
+        let status_report = this.name + ', '
+
+        if(status === 'active' && warp <= 4) {
+            status_report += 'the engines are active and we could be going faster.'
+        } else if (status === 'active' && warp > 4) {
+            status_report += 'the engines are active and we are going ' + warp + '.'
+        } else if (status === 'down') {
+            status_report += 'the engines are down.'
+        } else {
+            status_report += 'the comms are down and we can`t reach engineering.'
+        }
+
+        return status_report;
+    }
+
+    report() {
+        return `${this.determineStatus()}`
+        // using JavaScript template literal: `${}`
+        // \u00A0 is a symbol to add a space in the template literal
+    }
 }
 
-console.log(status_report)
+const status = new StatusReport("Captain")
+console.log(status.report())
