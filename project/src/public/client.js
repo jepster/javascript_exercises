7d968761c4ee7d8b3d4676646fbad70b1970a2cb
Peter Majmesku <majmesku@publicplan.de>
2021-03-16 19:28:01 +0100

    Able to run dev-stack with one command. Checkbox has a function. Backend is only queried once.

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -1,11 +1,55 @@
-const { fromJS } = require('immutable');
+const { fromJS, Map } = require('immutable');
 
 import 'bootstrap';
 import "./assets/stylesheets/main.scss";
 import 'js-loading-overlay'
+import _, { map } from 'underscore';
 
 // add our markup to the page
 const root = document.getElementById('root')
+
+window.roverDataRaw = null;
+
+let applicationStore = {};
+
+window.options = Map({
+    name: true,
+    imgSrc: true,
+    earthDate: true,
+    landingDate: true,
+    launchDate: true,
+    status: true,
+    camera: true
+});
+
+const enableOption = (option) => {
+    window.options = window.options.set(option, true);
+    render(root)
+}
+
+const disableOption = (option) => {
+    window.options = window.options.set(option, false);
+    render(root)
+}
+
+const updateApplicationStore = (roverDataRaw) => {
+    window.roverDataRaw = roverDataRaw;
+    const roverPhotoCollectionRaw = window.roverDataRaw.image.photos.sort((itemA, itemB) => itemA.earth_date - itemB.earth_date);
+
+    const roverPhotoCollection = roverPhotoCollectionRaw.map(item => {
+        return {
+            name: window.options.get('name') ? item.rover.name : null,
+            imgSrc: window.options.get('imgSrc') ? item.img_src : null,
+            earthDate: window.options.get('earthDate') ? item.earth_date : null,
+            landingDate: window.options.get('landingDate') ? item.rover.landing_date : null,
+            launchDate: window.options.get('launchDate') ? item.rover.launch_date : null,
+            status: window.options.get('status') ? item.rover.status : null,
+            camera: window.options.get('camera') ? item.camera.full_name : null,
+        };
+    });
+
+    return fromJS(roverPhotoCollection);
+}
 
 const render = async (root) => {
     root.innerHTML = await App();
@@ -14,45 +58,29 @@ const render = async (root) => {
     Array.from(inputElements).map(input => {
         input.addEventListener('change', (event) => {
             if (event.target.checked) {
-                console.log("Checkbox is checked..");
+                enableOption(event.target.id);
+                updateApplicationStore(window.roverDataRaw);
             } else {
-                console.log("Checkbox is not checked..");
+                disableOption(event.target.id);
+                updateApplicationStore(window.roverDataRaw);
             }
         });
     });
 
 }
 
-let applicationStore = {};
-
-const updateApplicationStore = (roverDataRaw) => {
-    const roverPhotoCollectionRaw = roverDataRaw.image.photos.sort((itemA, itemB) => itemA.earth_date - itemB.earth_date);
-
-    const roverPhotoCollection = roverPhotoCollectionRaw.map(item => {
-        return {
-            name: item.rover.name,
-            imgSrc: item.img_src,
-            earthDate: item.earth_date,
-            landingDate: item.rover.landing_date,
-            launchDate: item.rover.launch_date,
-            status: item.rover.status,
-            camera: item.camera.full_name
-        };
-    });
-
-    return fromJS(roverPhotoCollection);
-}
-
 // create content
-const App = async () => {
+const App = async (roverDataRaw) => {
 
     const fetchMoviesJSON = async () => {
         return await fetch('http://localhost:3000/apod').then(response => response.json());
     }
 
-    const roverDataRaw = await fetchMoviesJSON();
+    if (_.isEmpty(window.roverDataRaw)) {
+        window.roverDataRaw = await fetchMoviesJSON();
+    }
 
-    applicationStore = updateApplicationStore(roverDataRaw);
+    applicationStore = updateApplicationStore(window.roverDataRaw);
 
     return `${RenderedList(applicationStore)}`;
 }
@@ -103,8 +131,17 @@ const RenderedList = (immutableRoverPhotoCollection) => {
                             <div class="card selection-form">
                                 <form>
                                     <div class="form-check">
-                                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                        <input type="checkbox" class="form-check-input" id="landingDate" ` +
+
+      (() => {
+          if (window.options.get('landingDate') === true) {
+              return 'checked="checked"'
+          }
+      })()
+
+                                        + `/>
+                                        
+                                        <label class="form-check-label" for="landing_date">Landing date</label>
                                     </div>
                                 </form>
                             </div>
